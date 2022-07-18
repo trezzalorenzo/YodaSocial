1814f56e9d98a54f234b183a7019c3a1463d852f
trezzalorenzo <lorenzo.trezza@libero.it>
2022-07-18 23:47:05 +0200

    inizializzazione controller users

--- src/main/java/com/social/yodasocial/controller/UserController.java
+++ src/main/java/com/social/yodasocial/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.social.yodasocial.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    @GetMapping("/list")
+    public String getUsers(){
+        return "LIST USER";
+    }
+
+    @GetMapping("/user/{id}")
+    public String getUserById(@PathVariable int id, @RequestParam(required = false) String name){
+        if(name == null) return "ERRORE";
+        return "USER  " + id + " " +name;
+    }
 
 }
